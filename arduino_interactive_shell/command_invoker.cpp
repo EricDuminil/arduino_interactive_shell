9c079d122c31ebbaf9862cfb046163f78e88eae3
Eric Duminil <eric.duminil@gmail.com>
2021-05-03 00:06:37 +0200

    Add string commands as proof of concept

--- arduino_interactive_shell/command_invoker.cpp
+++ arduino_interactive_shell/command_invoker.cpp
@@ -14,8 +14,9 @@ namespace command_invoker {
   struct Command {
     const char *name;
     union {
-      void (*intFunction)(int32_t);
       void (*voidFunction)(void);
+      void (*intFunction)(int32_t);
+      void (*strFunction)(char*);
     };
     const char *doc;
     input_type parameter_type;
@@ -48,27 +49,34 @@ namespace command_invoker {
     }
   }
 
+  void defineStringCommand(const char *name, void (*function)(char*), const __FlashStringHelper *doc_fstring) {
+    if (addCommand(name, doc_fstring)) {
+      commands[commands_count].strFunction = function;
+      commands[commands_count++].parameter_type = STRING;
+    }
+  }
+
   /*
    * Tries to split a string command (e.g. 'mqtt 60' or 'show_csv') into a function_name and an argument.
-   * Returns the argument type.
+   * Returns the argument type, and defines the corresponding argument.
    */
-  input_type parseCommand(const char *command, char *function_name, int32_t &argument) {
+  input_type parseCommand(const char *command, char *function_name, int32_t &int_argument, char *str_argument) {
     char split_command[MAX_COMMAND_SIZE];
     strlcpy(split_command, command, MAX_COMMAND_SIZE);
-    char *arg;
     char *part1;
+    char *arg;
     part1 = strtok(split_command, " ");
     if (!part1) {
       Serial.println(F("Received empty command"));
       return NONE;
     }
     strlcpy(function_name, part1, MAX_COMMAND_SIZE);
-    arg = strtok(NULL, " ");
+    arg = strtok(NULL, "");
     if (arg) {
       char *end;
-      argument = strtol(arg, &end, 10);
+      int_argument = strtol(arg, &end, 10);
       if (*end) {
-        //TODO: Return arg
+        strlcpy(str_argument, arg, MAX_COMMAND_SIZE);
         return STRING;
       } else {
         return INT32;
@@ -99,24 +107,33 @@ namespace command_invoker {
    */
   void execute(const char *command_line) {
     char function_name[MAX_COMMAND_SIZE];
-    int32_t argument = 0;
+    char str_argument[MAX_COMMAND_SIZE];
+    int32_t int_argument = 0;
     input_type argument_type;
-    argument_type = parseCommand(command_line, function_name, argument);
-
+    argument_type = parseCommand(command_line, function_name, int_argument, str_argument);
     for (uint8_t i = 0; i < commands_count; i++) {
       if (!strcmp(function_name, commands[i].name) && argument_type == commands[i].parameter_type) {
         Serial.print(F("Calling : "));
         Serial.print(function_name);
-        if (argument_type == INT32) {
-          Serial.print(F("("));
-          Serial.print(argument);
-          Serial.println(F(")"));
-          commands[i].intFunction(argument);
-        } else {
+        switch (argument_type) {
+        case NONE:
           Serial.println(F("()"));
           commands[i].voidFunction();
+          break;
+        case INT32:
+          Serial.print(F("("));
+          Serial.print(int_argument);
+          Serial.println(F(")"));
+          commands[i].intFunction(int_argument);
+          break;
+        case STRING:
+          Serial.print(F("('"));
+          Serial.print(str_argument);
+          Serial.println(F("')"));
+          commands[i].strFunction(str_argument);
+          break;
+          return;
         }
-        return;
       }
     }
     Serial.print(F("'"));
