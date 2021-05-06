737107ceea623a94125b93da108c7d28fceff288
Eric Duminil <eric.duminil@gmail.com>
2021-05-06 10:21:34 +0200

    Better output.

--- arduino_interactive_shell/command_invoker.cpp
+++ arduino_interactive_shell/command_invoker.cpp
@@ -8,11 +8,18 @@ namespace command_invoker {
     int error;
     double result = te_interp(command_str, &error);
     if (error){
+        Serial.print(" ");
+        for(int i=0; i<error; i++){
+          Serial.print(" ");
+        }
+        Serial.println("↑");
         Serial.println("Je n'ai pas compris la question. :(");
         return;
     }
     Serial.print(" = ");
-    Serial.println(result);
+    char result_str[30];
+    snprintf(result_str, 30, "%.10g", result);
+    Serial.println(result_str);
     led_effects::showBinaryNumber(result);
 //    led_effects::showNumber(result);
   }
